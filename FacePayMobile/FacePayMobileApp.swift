41542ee7eac6497d55c3d6c947f897e37e3c2199
Jien Weng <reallyhat@gmail.com>
2025-07-31 13:32:35 +0800

    Initial Commit

---
+++ FacePayMobile/FacePayMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacePayMobileApp.swift
+//  FacePayMobile
+//
+//  Created by Lai Jien Weng on 31/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct FacePayMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
